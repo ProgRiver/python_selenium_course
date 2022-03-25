0854f21a59beb084b0fc070b4fe079ef06367227
Yury <100367090+ProgRiver@users.noreply.github.com>
2022-03-25 20:15:54 +0300

    Update test_items.py

--- test_items.py
+++ test_items.py
@@ -1,6 +1,6 @@
 from selenium.webdriver.common.by import By
 
 
-def test_add_to_cart_button_in_page(browser):
+def test_element_presence_control(browser):
     browser.get("https://stepik.org/")
     assert browser.find_element(By.CLASS_NAME, "navbar__submenu-toggler"), "[!] Элемент не найден [!]"
