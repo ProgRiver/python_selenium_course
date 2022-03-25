8ae77725d3321fe88af7b6afb588b1dd2ba3c54f
ProgRiver <barkas.ssr@mail.ru>
2022-03-25 12:05:41 +0300

    Structure building 1: add new files

--- test_items.py
+++ test_items.py
@@ -7,3 +7,5 @@ link = "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/"
 def test_add_to_cart_button_in_page(browser):
     browser.get(link)
     assert browser.find_elements(By.CLASS_NAME, "btn-add-to-basket"), "[*] There is no button on the page [*]"
+
+# pytest --language=es test_items.py
