11e9184cb4de491da02ce1e63cc23f08366c8973
ProgRiver <barkas.ssr@mail.ru>
2022-03-25 18:33:32 +0300

    Setting conftest: add two browsers and languages

--- conftest.py
+++ conftest.py
@@ -1,63 +1,34 @@
-# from selenium import webdriver
-
-# import pytest
-# import time
-
-# def pytest_addoption(parser):
-#     parser.addoption("--language", action="store", default="ru", help="Choose language")
-
-
-# @pytest.fixture(scope="function")
-# def browser(request):
-#     """
-#     Фикстура объявления браузера и настроек. Передается в тест, как параметр.
-#     """
-#     language_on_request = request.config.getoption("language")
-#     settings = webdriver.ChromeOptions()
-
-#     # Настройки запроса
-#     settings.add_experimental_option("prefs", {"intl.accept_languages": language_on_request})
-#     # # Настройки терминала (убирает лишнюю информацию)
-#     # settings.add_experimental_option('excludeSwitches', ['enable-logging'])
-
-#     driver = webdriver.Chrome(options=settings)
-#     yield driver
-
-#     # Установите ожидание закрытия браузера 
-#     time.sleep(3)
-#     driver.quit()
-
-
-
 from selenium import webdriver
 import pytest
 import time
 
 
 def pytest_addoption(parser):
-    parser.addoption('--browser_name', action='store', default="chrome",
+    parser.addoption("--browser_name", action="store", default="chrome",
                      help="Choose browser: chrome or firefox")
+    parser.addoption("--language", action="store", default="ru", help="Choose language")
 
 
 @pytest.fixture(scope="function")
 def browser(request):
 
-    settings = webdriver.ChromeOptions()
-    settings.add_experimental_option('excludeSwitches', ['enable-logging'])
-
     browser_name = request.config.getoption("browser_name")
+    browser_language = request.config.getoption("language")
+    
     driver = None
     if browser_name == "chrome":
-        driver = webdriver.Chrome(options=settings)
+        settings_ch = webdriver.ChromeOptions()
+        settings_ch.add_experimental_option("excludeSwitches", ['enable-logging'])
+        settings_ch.add_argument("--window-size=1250,950")
+        settings_ch.add_experimental_option("prefs", {'intl.accept_languages': browser_language})
+        driver = webdriver.Chrome(options=settings_ch)
     elif browser_name == "firefox":
-        driver = webdriver.Firefox()
+        settings_fp = webdriver.FirefoxOptions()
+        settings_fp.set_preference("intl.accept_languages", browser_language)
+        driver = webdriver.Firefox(options=settings_fp)
     else:
         raise pytest.UsageError("--browser_name should be chrome or firefox")
     
     yield driver
-    time.sleep(3)
+    time.sleep(2)
     driver.quit()
-
-
-# pytest -s -v --browser_name=firefox test_items.py
-# pytest -v --browser_name=firefox test_items.py
